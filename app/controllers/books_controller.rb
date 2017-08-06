4bc71bc7b037e7156ac4ba5c6c23b26f5b75c6cb
takezo <takezo.m.1987@gmail.com>
2017-08-06 20:21:12 +0900

    books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < ApplicationController
-  def index
+   def index
+    @books = Book.order(created_at: :desc)
+
+    #検索
+    @q = Book.page(params[:page]).search(params[:q])
+    @searchedBooks = @q.result(distinct: true)
+
+    @title = "レシピ一覧"
+
+
   end
 end
